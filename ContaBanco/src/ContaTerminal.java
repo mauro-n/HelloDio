c41b2ba0b207a8f5ad5cf1c1d1842768174865df
test <mauro.henrique613@gmail.com>
2023-05-07 13:14:32 -0300

    Project: ContaBanco -- trilha Formacao Java Developer

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,41 @@
+/**
+ * <h1>ContaTerminal</h1>
+ * <p>
+ * A classe ContaTerminal tem como objetivo solucionar o desafio
+ * da DIO para demonstrar conhecimento da sintaxe básica JAVA.
+ * </p>
+ * @author mauro-n
+ * @since 07/05/2023
+ */
+import java.util.Scanner;
+public class ContaTerminal {
+	/**
+	 * Programa vai solicitar nome String, numeroConta int, agencia String
+	 * e saldo double. Então imprime uma string formatada mostrando os dados.
+	 * @param args sem args.
+	 */
+	public static void main(String[] args) {		
+		int numeroConta;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+		Scanner stringInput = new Scanner(System.in);
+		Scanner numericInput = new Scanner(System.in);
+		
+		System.out.println("Por favor, Insira seu nome: ");
+		nomeCliente = stringInput.nextLine();
+		
+		System.out.println("Por favor, Insira seu número de conta: ");
+		numeroConta = numericInput.nextInt();
+		
+		System.out.println("Por favor, Insira sua Agencia: ");
+		agencia = stringInput.nextLine();
+		
+		System.out.println("Por favor, Insira seu saldo: ");
+		saldo = numericInput.nextDouble();
+		
+		System.out.printf("Olá, %s, obrigado por criar uma conta em nosso banco%n"
+				+ "sua agência é %s, conta %s e seu saldo %.2f já está disponivel"
+				+ " para saque.", nomeCliente, agencia, numeroConta, saldo);
+	}
+}
